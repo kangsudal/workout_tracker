c9042ff4f17f7c92552341e6b052564f7b2aa10c
kangsudal <kangsudal@gmail.com>
2024-10-16 15:32:42 +0900

    FadeTransition, 랜딩페이지

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -3,9 +3,34 @@ import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:workout_tracker/my_router.dart';
 
-
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,39 +48,42 @@ class LandingPage extends StatelessWidget {
               opacity: 0.5,
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.all(8),
-                color: Colors.yellowAccent,
-                child: Text(
-                  '남들이 그만둘 때, 난 계속한다',
-                  style: GoogleFonts.nanumBrushScript(fontSize: 20),
+          child: FadeTransition(
+            opacity: _animation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  color: Colors.yellowAccent,
+                  child: Text(
+                    '남들이 그만둘 때, 난 계속한다',
+                    style: GoogleFonts.nanumBrushScript(fontSize: 20),
+                  ),
                 ),
-              ),
-              Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 80),
-                    child: Text(
-                      '환영합니다',
-                      style: GoogleFonts.jua(fontSize: 30),
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 80),
+                      child: Text(
+                        '환영합니다',
+                        style: GoogleFonts.jua(fontSize: 30),
+                      ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      context.go('/workout_home');
-                    },
-                    child: Text(
-                      'Start',
-                      style: TextStyle(fontSize: 35),
+                    ElevatedButton(
+                      onPressed: () {
+                        context.go('/workout_home');
+                      },
+                      child: Text(
+                        'Start',
+                        style: TextStyle(fontSize: 35),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
